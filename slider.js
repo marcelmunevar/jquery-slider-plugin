c89301108c956df4caacf36e1eae54729540e643
Marcel Munevar <Killeramoeba@gmail.com>
2014-01-29 22:56:10 -0500

    Simplified Style Sheet
    Style sheet simplified. Some style values are now calculated in
    JavaScript file.

--- slider.js
+++ slider.js
@@ -1,173 +1,196 @@
 (function( $ ){
 
-  $.fn.slider = function( leftButton, rightButton,  options) {  
-  
-    // Create some defaults, extending them with any options that were provided
-    var settings = $.extend( {
-      'numChildrenPerPage'                : '1',
-      'clipEdge'                        : 'true'
-    }, options);
-
-
-
-
-         
-    return this.each(function() {
-                var object = $(this);
-                var numButtons = countButtons(object.children());
-                var numPages = Math.ceil(numButtons/settings.numChildrenPerPage);
-                //var widthChildren = Math.ceil(object.width()/settings.numChildrenPerPage);
-				var currentPage = 1;
-                var adjustment = Math.ceil(object.width()/settings.numChildrenPerPage)*settings.numChildrenPerPage-object.width();
-				
-				
-				
-                function buttons() {
-                        var buttonsAreActive = true; // local variable
-                        
-                        var  temp = function(action) 
-                        {
-                                if(action == "swap")
-                                {
-                                        buttonsAreActive = !buttonsAreActive; 
-                                }
-                                else
-                                {
-                                        return buttonsAreActive; 
-                                }
-                        }
-                        return temp;
-                }
-                
-                var buttons = buttons();
-                
-                
-				//get width for outer
-				//get width for inner.
-                var widthOfOuter = 0;               
-                var widthOfInner = 0;
-                object.children().children().each(function (i) {
-                        
-                        widthOfInner += $(this).width();
-						widthOfInner += parseInt($(this).css("margin-right"));
-						widthOfInner += parseInt($(this).css("padding-left"));
-						widthOfInner += parseInt($(this).css("padding-right"));
-						if( i == settings.numChildrenPerPage-1 )
-						{
-							widthOfOuter = widthOfInner;
-						}
-						
-                });
-				
-								
-				widthOfOuter -= adjustment;
+	$.fn.slider = function( leftButton, rightButton,  options) {  
+	
+		// Create some defaults, extending them with any options that were provided
+		var settings = $.extend( {
+			'numChildrenPerPage'	:	'1',
+			'clipEdge'	:	'true'
+		}, options);
+		
+		
+		
+		
+			 
+		return this.each(function() {
+					//static variables
+					var outerDiv = $(this);
+					var innerDiv = outerDiv.children();
+					var galleryDiv = outerDiv.parent();
+					var tiles = innerDiv.children();
+					var imgObjects = tiles.children();
+					var imgMargin = parseFloat(imgObjects.css("margin-right"));
+					var numButtons = tiles.length;
+					var numPages = Math.ceil(numButtons/settings.numChildrenPerPage);
+					//var adjustment = Math.ceil(outerDiv.width()/settings.numChildrenPerPage)*settings.numChildrenPerPage-outerDiv.width();
+					//var widthChildren = Math.ceil(outerDiv.width()/settings.numChildrenPerPage); //this is no longer used but can be useful when debugging
 					
+					//dynamic variables
+					var buttonSwitch = buttons();  //button switch is encapsulated in a closure
+					var currentPage = 1;
+					var widthOfOuter = 0;
+					var widthOfInner = 0;
 					
-				if(widthOfOuter  <= object.parent().width())
-				{
-					//set width for inner and outer
-					object.css("width",widthOfOuter).children().css("width",widthOfInner);
-					
-					
-					
-					if(numPages > 1)
+					//calculate and set some css values
+					if(imgMargin == 0)
 					{
-							updateButtonStatus(currentPage,numPages, leftButton, rightButton);        
+						outerDiv.css("margin-top", outerDiv.css("padding-top") );
+						outerDiv.css("margin-bottom", outerDiv.css("padding-bottom") );
+						outerDiv.css("padding-top", 0);
+						outerDiv.css("padding-bottom", 0);
+						outerDiv.css("box-shadow", imgObjects.css("box-shadow"));
+						imgObjects.css("box-shadow", "none");
 					}
 					else
 					{
-							leftButton.css("visibility","hidden");
-							rightButton.css("visibility","hidden");
-					}        
+						innerDiv.css("padding-left", imgMargin/2);
+					}					
+					var a = galleryDiv.height()/2;
+					var b = leftButton.height()/2;
+					leftButton.css("top", (a)-(b));
+					rightButton.css("top", (a)-(b));
+					rightButton.css("right", leftButton.css("left"));
+										
+					//calculate the width of innerDiv and outerDiv
+					tiles.each(function (i) {						
+							widthOfInner += $(this).width();
+							widthOfInner += parseFloat($(this).css("margin-right"));
+							widthOfInner += parseFloat($(this).css("border-left-width"));
+							widthOfInner += parseFloat($(this).css("border-right-width"));
+							console.log()
+							//set widthOfOuter after first page is calculated
+							if( i == settings.numChildrenPerPage-1 )
+							{
+								widthOfOuter = widthOfInner;
+							}
+					});			
 					
-					leftButton.click(function() {
-							currentPage = pan(currentPage,numPages,-1, buttons, object, adjustment, settings.clipEdge);
-							updateButtonStatus(currentPage,numPages, leftButton, rightButton);        
-					});
-					rightButton.click(function() {
-							currentPage = pan(currentPage,numPages,1, buttons, object, adjustment, settings.clipEdge);
-							updateButtonStatus(currentPage,numPages, leftButton, rightButton);        
-					});
+					//calculate space available for gallery
+					spaceAvailable = (((parseFloat(leftButton.css("left"))+parseFloat(leftButton.css("width")))*2)-parseFloat(galleryDiv.width()))*-1;
+					console.log("a"+widthOfOuter);
+					console.log("b"+spaceAvailable);
 					
-					object.find("img").css("visibility","visible");
+					//make sure there is enough space in parent
+					if(widthOfOuter  <= spaceAvailable)
+					{
+						//set width for inner and outer
+						outerDiv.css("width",widthOfOuter);//-adjustment);
+						innerDiv.css("width",widthOfInner);			
+						
+						//if there is more than one page
+						if(numPages > 1)
+						{	
+								//activate buttons
+								updateButtonStatus(currentPage,numPages, leftButton, rightButton);  
+								leftButton.click(function() {
+									currentPage = pan(currentPage,numPages,-1, buttonSwitch, widthOfOuter, widthOfInner, innerDiv, settings.clipEdge);
+									updateButtonStatus(currentPage,numPages, leftButton, rightButton);        
+								});
+								rightButton.click(function() {
+									currentPage = pan(currentPage,numPages,1, buttonSwitch, widthOfOuter, widthOfInner, innerDiv, settings.clipEdge);
+									updateButtonStatus(currentPage,numPages, leftButton, rightButton);        
+								});							      
+						}
+						else
+						{
+								leftButton.css("visibility","hidden");
+								rightButton.css("visibility","hidden");
+						}        
+						
+						//make images visible
+						imgObjects.css("visibility","visible");
+					}
+					else
+					{
+						outerDiv.html("<p style='text-align:center;'><br><br>The slider won't fit in the gallery div.<br>Either reduce numChildrenPerPage or increase the width of the gallery.</p>");
+					}
+		
+		});
+		
+		//encapsulated button switch
+		function buttons() {
+			var buttonsAreActive = true; // local variable
+			
+			var  temp = function(action) 
+			{
+					if(action == "swap")
+					{
+							buttonsAreActive = !buttonsAreActive; 
+					}
+					else
+					{
+							return buttonsAreActive; 
+					}
+			}
+			return temp;
+		}
+		
+		//checks and updates button visibility
+		function updateButtonStatus(currentPage,numPages,leftButton,rightButton)
+		{
+				if(currentPage == numPages)
+				{
+						rightButton.css("visibility","hidden");
+						leftButton.css("visibility","visible");
+				}
+				else if(currentPage == 1)
+				{
+						leftButton.css("visibility","hidden");
+						rightButton.css("visibility","visible");
 				}
 				else
 				{
-					object.html("<p style='text-align:center;'><br><br>Too many numChildrenPerPage.<br>Either reduce numChildrenPerPage or increase the width of the gallery.</p>");
+						leftButton.css("visibility","visible");
+						rightButton.css("visibility","visible");
 				}
-
-    });
-        
-        
-        function countButtons(inner)
-        {
-                var numButtons = 0;
-                inner.children().each(function () {
-                        numButtons += 1;
-                });                                
-                return numButtons;
-        }
-        function updateButtonStatus(currentPage,numPages,leftButton,rightButton)
-        {
-                if(currentPage == numPages)
-                {
-                        rightButton.css("visibility","hidden");
-                        leftButton.css("visibility","visible");
-                }
-                else if(currentPage == 1)
-                {
-                        leftButton.css("visibility","hidden");
-                        rightButton.css("visibility","visible");
-                }
-                else
-                {
-                        leftButton.css("visibility","visible");
-                        rightButton.css("visibility","visible");
-                }
-        }
-        function pan(currentPage, numPages, pages, buttons, object, adjustment, clipEdge)
-        {
-                if(currentPage+pages > 0 && currentPage+pages <= numPages && buttons())
-                {
-                        buttons("swap");
-                        if(clipEdge == "true")
-                        {
-                                if(currentPage == numPages-1 && pages > 0)
-                                {
-                                        distance = 0-object.children().width()+object.width()+adjustment;
-                                }
-                                else if(currentPage == 2 && pages < 0)
-                                {
-                                        distance = 0;
-                                }
-                                else
-                                {
-                                        distance = "-="+(object.width()+adjustment)*pages+"px";
-                                }
-                        }
-                        else
-                        {
-                                distance = "-="+(object.width()+adjustment)*pages+"px";
-								
-                        }
-                        newPage = currentPage+pages;
-                        object.children().animate({"left": distance}, "slow",
-                                function() 
-                                {
-                                        
-                                        buttons("swap");
-                                }                                        
-                        );
+		}
+		
+		//when a user clicks a button
+		function pan(currentPage, numPages, pages, buttonSwitch, widthOfOuter, widthOfInner, innerDiv, clipEdge)
+		{
+				
+				if(buttonSwitch())
+				{
+						//deactivate buttons
+						buttonSwitch("swap");
 						
-                        return newPage;
-                }
-                else
-                {
-                        return currentPage;
-                }
-                
-        }
-        
-
-  };
+						
+						if(clipEdge == "true")
+						{
+							//if second to last page and panning right		
+							if(currentPage == numPages-1 && pages > 0)
+							{
+									distance = 0-widthOfInner+widthOfOuter;
+							}
+							//if second page and panning left
+							else if(currentPage == 2 && pages < 0)
+							{
+									distance = 0;
+							}
+							else
+							{
+									distance = "-="+(widthOfOuter)*pages+"px";
+							}
+						}
+						else
+						{
+								distance = "-="+(widthOfOuter)*pages+"px";
+								
+						}
+						
+						//set newPage
+						newPage = currentPage+pages;
+						
+						//animate left at calculated distance at a slow speed, reactivate buttons on completion
+						innerDiv.animate({"left": distance}, "slow",	function(){ buttonSwitch("swap"); });
+						
+						return newPage;
+				}
+				
+				return currentPage;
+				
+		}
+		
+	
+	};
 })( jQuery );
